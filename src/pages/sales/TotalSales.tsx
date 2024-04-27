ead8ad7b722bb735f3e621fa90ceb813cf63265e
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2024-04-27 18:56:32 +0800

    adding partial icons in every component

--- src/pages/sales/TotalSales.tsx
+++ src/pages/sales/TotalSales.tsx
@@ -4,6 +4,7 @@ const TotalSales = () => {
     return (
       <div>
         <CardHeader>Total Sales</CardHeader>
+        <div className="text-6xl font-bold mt-10">₱ 15.25</div>
       </div>
     );
   };
