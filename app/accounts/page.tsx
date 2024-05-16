41cafea7dc78b8d4ade8d3d85df2ea840a111ed0
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2024-05-16 16:31:29 +0800

    Revert "can add member user role"
    This reverts commit 0c773b3bec2d08f54478a12a6b28d9a9a238704a.

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,16 +1,7 @@
 import { isLoggedIn } from "@/lib/utils";
 import React from "react";
-import SignUpMember from "./AddMember";
-import { Toaster } from "@/components/ui/toaster";
 
 export default async function Accounts() {
   await isLoggedIn();
-  return (
-    <main className="w-full">
-      <div>
-        <Toaster />
-        <SignUpMember />
-      </div>
-    </main>
-  );
+  return <div className="w-full">Accounts</div>;
 }
